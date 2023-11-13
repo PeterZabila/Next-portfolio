e957952dcf7e1383d1a40dccceb9cf45dd658633
PeterZabila <petrozabila@gmail.com>
2023-11-13 17:40:54 +0200

    about page done footer styles fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 import { footerLinks } from "../constants"
 
 const Footer = () => (
-    <footer className=' text-black-100  mt-5 border-t border-gray-200'>
-      <div className='flex max-md:flex-row flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+    <footer className='w-full mx-auto text-black-100  mt-5 border-t border-gray-200'>
+      <div className='max-w-[1440px] flex max-md:flex-row flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
         <div className='flex flex-col justify-start items-start gap-6'>
           <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
           <p className='text-base text-gray-700'>
